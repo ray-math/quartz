0f4861f2a97ad0032e872f1a33eb3a8cc80da042
Ray <ray_c20@naver.com>
2025-02-10 14:23:04 +0900

    vault backup: 2025-02-10 14:23

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Ray",
+    pageTitle: "Rayㅤㅤ",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
